fff9a885b1067f0e6aad7957d61468181ed92c83
iffat-siddick <k22031129@kcl.ac.uk>
2023-10-26 19:59:27 +0100

    Try 1 of task done

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,17 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
-
 def frequencies(items):
-    frequencies = {}
-    # Your code goes here
+    frequencies = {} #dictionary with (key, value pairs)
+    x = 0
+    keys = []
+    for x in items:
+        frequencies.update({str(x):0})
+        valueSum = 0
+        for y in items:
+            if (y == x):
+                valueSum += 1
+        frequencies.update({str(x):valueSum})
     return frequencies
+
+dictionary = frequencies([1,2,2,2,3,3,4,4,4,6,6,6,66,6])
+print(dictionary)
